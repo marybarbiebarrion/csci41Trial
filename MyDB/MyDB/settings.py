3d99d3304be3e8cc8bfdc9c2acb71f6d4b292748
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-11-11 02:04:13 +0800

    setup again

--- MyDB/MyDB/settings.py
+++ MyDB/MyDB/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b=59ntxwti)hd$f)(rtj&g#285-ud3viu41+34rm(ep_ged^9='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
