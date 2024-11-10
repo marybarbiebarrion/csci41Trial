fb8ba24abca53f5263b068629f66545e3fca4583
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-11-11 01:48:03 +0800

    updates DATABASES on settings.py to house mysql

--- MyDB/MyDB/settings.py
+++ MyDB/MyDB/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'MyDB.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mydb',
+		'USER': 'root',
+		'PASSWORD': 'mysql',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
