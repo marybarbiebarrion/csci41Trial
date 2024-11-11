935a2b03f25e2d174bb4a47aebe7daab224d0dbc
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-11-11 18:43:16 +0800

    Create forms.py

---
+++ MyDB/centralbookings/forms.py
@@ -0,0 +1,12 @@
+# centralbookings/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'role')
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass
