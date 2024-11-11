9d402787df46b21f9dc8c8e28d3676c15a9e1fa8
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-11-11 18:09:45 +0800

    Update models.py
    tried to create a customer registration whether a participant or organizer

--- MyDB/centralbookings/models.py
+++ MyDB/centralbookings/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser): 
+    PARTICIPANT = 'participant' 
+    ORGANIZER = 'organizer' 
+    
+    ROLE_CHOICES = [ 
+        (PARTICIPANT, 'Participant'), 
+        (ORGANIZER, 'Organizer'), 
+    ] 
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
