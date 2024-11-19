099304b104331d64074c74724ea4e2137c094387
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-11-20 03:57:05 +0800

    Update forms for participant model

--- MyDB/centralbookings/forms.py
+++ MyDB/centralbookings/forms.py
@@ -1,13 +1,14 @@
 # centralbookings/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import CustomUser, Organizer, ContactPerson
+from .models import CustomUser, Organizer, ContactPerson, Participant
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('username', 'email', 'role')
+        fields = ('username', 'email')
 
+#        fields = ('username', 'email', 'role')
 class CustomAuthenticationForm(AuthenticationForm):
     pass
 
@@ -20,3 +21,11 @@ class ContactPersonForm(forms.ModelForm):
     class Meta:
         model = ContactPerson
         fields = ['Contact_Name', 'Contact_Email', 'Contact_Number']
+
+class ParticipantForm(forms.ModelForm):
+    class Meta:
+        model = Participant
+        fields = ['Participant_Name', 'Birth_Date', 'Department', 'Participant_Type']
+        widgets = { 
+            'Birth_Date': forms.DateInput(attrs={'type': 'date'}), 
+        }
