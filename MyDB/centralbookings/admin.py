ca166f72958649f9dbf6ae017dd1e57db33a0c92
Mary Barbie Barrion <mary.barrion@student.ateneo.edu>
2024-11-29 10:32:55 +0800

    Added participant and userparticipant linkage in the admin.py

--- MyDB/centralbookings/admin.py
+++ MyDB/centralbookings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser, Organizer, UserOrganizer
+from .models import CustomUser, Organizer, UserOrganizer, Participant, UserParticipant
 
 admin.site.register(CustomUser)
 admin.site.register(Organizer)
 admin.site.register(UserOrganizer)
+admin.site.register(Participant)
+admin.site.register(UserParticipant)
